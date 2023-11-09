ef91cbaf19056f7d63635f4ef58b4c34c251f8ed
parkjeongjoon <whdwns5060@naver.com>
2023-11-09 20:11:35 +0900

    로그인과 로그아웃

--- web-board/src/main/java/com/gdsc/webboard/user/UserController.java
+++ web-board/src/main/java/com/gdsc/webboard/user/UserController.java
@@ -17,11 +17,10 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/signup")
-    public String signup(UserCreateForm userCreateForm) {
-        return "signup_form";
+    @GetMapping("/login")
+    public String login() {
+        return "login_form";
     }
-
     @PostMapping("/signup")
     public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
@@ -46,8 +45,6 @@ public class UserController {
             bindingResult.reject("signupFailed", e.getMessage());
             return "signup_form";
         }
-
-
         return "redirect:/";
     }
 }
